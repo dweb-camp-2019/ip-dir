984b467899e571f969dd3fe2eaed0879aeea7c41
Alex Alekseyenko <alexalekseyenko@gmail.com>
2019-06-27 17:39:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# ip-dir
+Simple webapp for keeping track of IP addresses at DWeb Camp. Security is nonexistent; anyone can post anything.
+- Logs written to `ip-dir.log`
+- Records written to `records.json`
+
+# Usage
+First, install the dependencies:
+
+    npm install
+
+Then, start the app:
+
+    npm start
+
+If you are developing and would like the app to restart on file changes:
+
+    npm run dev
+
+# TODOs
+- write tests
+- validate input
---
+++ index.js
@@ -0,0 +1,121 @@
+const fs = require('fs')
+const express = require('express')
+const { createLogger, format, transports } = require('winston')
+
+const APPNAME = 'ip-dir'
+const FILEPATH = 'records.json'
+const FIELDS = ['ip', 'name', 'description']
+const PORT = 3000
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.errors({ stack: true }),
+    format.timestamp(),
+    format.splat(),
+    format.json(),
+  ),
+  transports: [
+    new transports.File({
+      filename: `${APPNAME}.log`
+    }),
+    new transports.Console({
+      format: format.combine(
+        format.colorize(),
+        format.printf(
+          ({ level, message, timestamp }) => `${timestamp} ${level}: ${message}`
+        ),
+      )
+    })
+  ]
+})
+
+fs.exists(FILEPATH, (exists) => {
+  if (!exists) {
+    fs.writeFile(FILEPATH, JSON.stringify([]), () => {
+      logger.info(`Writing new file: ${FILEPATH}`)
+    })
+  }
+})
+
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  fs.readFile(FILEPATH, (err, data) => {
+    if (err) {
+      logger.error(`Error reading file:\n${err}`)
+    }
+    res
+      .set('Content-Type', 'text/html')
+      .status(200)
+      .send(`
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <title>${APPNAME}</title>
+      <style>
+        body {
+          font-family: monospace;
+        }
+        input {
+          margin-right: 2em;
+        }
+        table {
+          margin-top: 2em;
+        }
+        th {
+          padding-bottom: 0.5em;
+        }
+        th, td {
+          padding-right: 2em;
+        }
+      </style>
+   </head>
+   <body>
+        <div>
+            <form action="/" method="POST">
+                ${FIELDS.map(
+                  (field) => `${field}:<input type="text" name="${field}">`).join('')
+                }
+                <input type="submit" value="Append">
+            </form>
+            <table>
+                <tr>
+                    ${FIELDS.map(
+                      (field) => `<th align="left">${field}</th>`).join('')
+                    }
+                </tr>
+                ${JSON.parse(data).map(
+                  (record) => `<tr>${FIELDS.map(
+                    (field) => `<td>${record[field]}</td>`).join('')
+                  }</tr>`).join('\n')}
+            </table>
+        </div>
+        <script type="text/javascript">
+        </script>
+    </body>
+</html>
+`)})
+})
+
+app.post('/', (req, res) => {
+  const record = Object.assign({}, ...FIELDS.map((field) => ({[field]: req.body[field]})))
+  var records
+  fs.readFile(FILEPATH, (err, data) => {
+    records = JSON.parse(data)
+    logger.info(`Appending new record:\n\t${JSON.stringify(record)}`)
+    records.push(record)
+    fs.writeFile(FILEPATH, JSON.stringify(records), (err) => {
+      if (err) {
+        logger.error(`Error writing file:\n${err}`)
+      }
+      res.redirect('/')
+    })
+  })
+})
+
+app.listen(PORT, () => {
+  logger.info(`${APPNAME} listening on port ${PORT}!`)
+})
